44f6e4f44ae42eaa0df247699015602217d7ec8d
Pierre Schmitz <pierre@archlinux.de>
2011-01-23 12:03:46 +0100

    Fix scope and namespace issues in our navbar template

--- style/ArchLinux/archnavbar.php
+++ style/ArchLinux/archnavbar.php
@@ -1,13 +1,14 @@
+<?php global $arch_home, $arch_navbar, $arch_navbar_selected; ?>
 <div id="archnavbar" class="anb-forum">
 	<div id="archnavbarlogo"><h1><a href="<?php if (isset($arch_home)) { echo $arch_home; } ?>">Arch Linux</a></h1></div>
 	<div id="archnavbarmenu">
 		<ul id="archnavbarlist">
 			<?php
 			if (isset($arch_navbar)) {
-				foreach ($arch_navbar as $name => $url) {
-					echo '<li id="anb-'.strtolower($name).'"'
-						.(isset($arch_navbar_selected) && $arch_navbar_selected == $name ? ' class="anb-selected"' : '')
-						.'><a href="'.$url.'">'.$name.'</a></li>';
+				foreach ($arch_navbar as $arch_name => $arch_url) {
+					echo '<li id="anb-'.strtolower($arch_name).'"'
+						.(isset($arch_navbar_selected) && $arch_navbar_selected == $arch_name ? ' class="anb-selected"' : '')
+						.'><a href="'.$arch_url.'">'.$arch_name.'</a></li>';
 				}
 			}
 			?>
