e974d6ccd4df9144df4197e9675502f924d3e60f
Pierre Schmitz <pierre@archlinux.de>
2017-08-30 06:35:51 +0200

    Optimize menu

--- style/ArchLinux/css.php
+++ style/ArchLinux/css.php
@@ -1,10 +1,7 @@
 <?php
 
-$cssSums = '';
-
 foreach (array('arch', 'archnavbar') as $cssFile) {
-    $cssSums .= sha1_file(__DIR__ . '/' . $cssFile . '.css');
     ?>
-    <link rel="stylesheet" media="screen" href="style/ArchLinux/<?= $cssFile ?>.css?v=<?= sha1($cssSums) ?>"/>
+    <link rel="stylesheet" media="screen" href="style/ArchLinux/<?= $cssFile ?>.css?v=4"/>
     <?php
 }
